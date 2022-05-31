ad3529c6caaa9ed467e9ad44a91c940671e443fd
Jan Aagaard <jan@aagaard.net>
2022-05-31 21:29:01 +0200

    Add empty lines

--- __tests__/exampleRendering.test.jsx
+++ __tests__/exampleRendering.test.jsx
@@ -1,7 +1,7 @@
-import React from "react";
 import { render } from "@testing-library/react";
-import { clickOnComponent } from "./utils";
 import App from "example/src/App";
+import React from "react";
+import { clickOnComponent } from "./utils";
 
 describe("Example view", () => {
   describe("When example view has been rendered", () => {
@@ -24,34 +24,42 @@ describe("Example view", () => {
       environment.getImgElement = () => getByAltText("example-image");
       environment.getTextElement = () => getAllByText(/Lorem ipsum/)[0];
     });
+
     test("it renders entire example without errors", () => {
       const { container } = environment.component;
       expect(container).toBeDefined();
     });
+
     test("it renders toggle button without errors", () => {
       const { toggleBtn } = environment;
       expect(toggleBtn).toBeDefined();
     });
+
     test("it renders zoom in button without errors", () => {
       const { zoomInBtn } = environment;
       expect(zoomInBtn).toBeDefined();
     });
+
     test("it renders zoom out button without errors", () => {
       const { zoomOutBtn } = environment;
       expect(zoomOutBtn).toBeDefined();
     });
+
     test("it renders reset button without errors", () => {
       const { resetBtn } = environment;
       expect(resetBtn).toBeDefined();
     });
+
     test("it renders transform component without errors", () => {
       const { transformComponent } = environment;
       expect(transformComponent).toBeDefined();
     });
+
     test("it renders image element without errors", () => {
       const { getImgElement } = environment;
       expect(getImgElement()).toBeDefined();
     });
+
     test("it renders text element without errors when toggle button get clicked", () => {
       const { getTextElement, toggleBtn } = environment;
       clickOnComponent(toggleBtn);

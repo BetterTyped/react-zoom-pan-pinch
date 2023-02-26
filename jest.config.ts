602e8273b236fbedcc6f4fa94f7acfcf396659e6
Maciej Pyrc <maciekpyrc@gmail.com>
2023-02-26 15:35:23 +0100

    test: 💍 Tests setup

--- jest.config.ts
+++ jest.config.ts
@@ -5,11 +5,23 @@ const config: Config.InitialOptions = {
   testEnvironment: "jsdom",
   preset: "ts-jest",
   testRegex: [".spec.ts"],
-  roots: ["<rootDir>/__tests__", "<rootDir>/src"],
+  roots: ["<rootDir>/node_modules", "<rootDir>/src"],
   coverageProvider: "v8",
-  coverageReporters: [["lcov", { projectRoot: "../.." }], "clover", "json", "text"],
+  coverageReporters: [
+    ["lcov", { projectRoot: "../.." }],
+    "clover",
+    "json",
+    "text",
+  ],
   collectCoverageFrom: ["<rootDir>/src/**/*.ts", "<rootDir>/src/**/*.tsx"],
-  coveragePathIgnorePatterns: [".spec", "test", "tests", "types", "constants", "index.ts"],
+  coveragePathIgnorePatterns: [
+    ".spec",
+    "test",
+    "tests",
+    "types",
+    "constants",
+    "index.ts",
+  ],
   moduleDirectories: ["node_modules", "src"],
   transform: {
     "^.+\\.tsx?$": [
@@ -20,11 +32,13 @@ const config: Config.InitialOptions = {
       },
     ],
   },
-  watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"],
-  setupFilesAfterEnv: ["jest-extended/all"],
   moduleNameMapper: {
-    "@browser-adapter": ["<rootDir>/src/adapter/adapter.browser.ts"],
-    "@server-adapter": ["<rootDir>/src/adapter/adapter.server.ts"],
+    "\\.(css|less|scss|sss|styl)$": "<rootDir>/node_modules/jest-css-modules",
   },
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname",
+  ],
+  setupFilesAfterEnv: ["jest-extended/all"],
 };
 export default config;

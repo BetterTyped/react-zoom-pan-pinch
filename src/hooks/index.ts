042c2832676a7a2b15537114bc19d02d872337aa
Tyler <26290074+tylerlaws0n@users.noreply.github.com>
2024-06-20 14:38:46 -0700

    feat(transform-hook): UseTransformComponent added (#405)
    Hook added to return an element, given the current state
    
    Co-authored-by: Tyler Lawson <tyler@secondspectrum.com>

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@ export * from "./use-controls";
 export * from "./use-transform-init";
 export * from "./use-transform-effect";
 export * from "./use-transform-context";
+export * from "./use-transform-component";

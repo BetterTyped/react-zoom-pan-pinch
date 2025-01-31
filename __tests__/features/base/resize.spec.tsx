b3a50850afa777239451a8b6721b4521787a2f4b
Maciej Pyrc <maciekpyrc@gmail.com>
2025-01-31 11:56:25 +0100

    tests

---
+++ __tests__/features/base/resize.spec.tsx
@@ -0,0 +1,8 @@
+describe("Base [Resize]", () => {
+  describe("When wrapper is resized", () => {
+    it("should align to bounds", () => {
+      // TODO: Implement test
+      expect(true).toBe(true);
+    });
+  });
+});
